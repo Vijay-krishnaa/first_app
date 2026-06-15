59b689f6e7d4b4fc6cdb67ca5f83b5aa84821663
Vijay Krishna <vkk7739@gmail.com>
2026-06-15 17:16:11 +0530

    routing and nested routing

---
+++ app/services/full-dev/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>this is full dev page</div>;
+}
+
+export default page;
--- app/services/page.js
+++ app/services/page.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 function page() {
-  return <div>All Services</div>;
+  return (
+    <div>
+      All Services
+      <br />
+      <Link href="/">Home</Link>
+      <br />
+      <Link href="/services/web-dev">web-dev</Link>
+      <br />
+      <Link href="/services/full-dev">full-dev</Link>
+    </div>
+  );
 }
 
 export default page;
